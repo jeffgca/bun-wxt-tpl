73561be205f0172d7cdd2df0fa4e21a16adb7c11
Jeff Griffiths <jeff@eko-recordings.ca>
2026-03-08 17:27:20 -0700

    working onclick

--- src/entrypoints/background/index.ts
+++ src/entrypoints/background/index.ts
@@ -4,30 +4,26 @@ export default defineBackground(() => {
 	console.log('Hello background!', { id: browser.runtime.id })
 
 	browser.action.onClicked.addListener(async (event) => {
-		console.log('clicked!', { event })
+		const tabUrl = browser.runtime.getURL('/tab.html')
 
-		;async () => {
-			const tabUrl = browser.runtime.getURL('/tab.html')
+		console.log('clicked', event)
 
-			// Query all tabs to find if one with our page is already open
-			const tabs = await browser.tabs.query({})
-			const existingTab = tabs.find((tab) => tab.url === tabUrl)
+		// Query all tabs to find if one with our page is already open
+		const tabs = await browser.tabs.query({})
+		const existingTab = tabs.find((tab) => tab.url === tabUrl)
 
-			if (existingTab && existingTab.id) {
-				// Tab already open - switch to it
-				await browser.tabs.update(existingTab.id, { active: true })
-				// Also bring the window to focus if the tab is in a different window
-				if (existingTab.windowId) {
-					await browser.windows.update(existingTab.windowId, {
-						focused: true,
-					})
-				}
-			} else {
-				// Tab not open - create a new one
-				await browser.tabs.create({
-					url: tabUrl,
-				})
+		if (existingTab && existingTab.id) {
+			// Tab already open - switch to it
+			await browser.tabs.update(existingTab.id, { active: true })
+			// Also bring the window to focus if the tab is in a different window
+			if (existingTab.windowId) {
+				await browser.windows.update(existingTab.windowId, { focused: true })
 			}
+		} else {
+			// Tab not open - create a new one
+			await browser.tabs.create({
+				url: tabUrl,
+			})
 		}
 	})
 })
